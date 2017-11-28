2bdd0c5d6e35d1258b013cd52b6589fa0cedde08
eparlato <edoardo.parlato@astir.com>
2017-11-28 09:10:51 +0100

    wrote a bunch of e2e tests (just the description)

--- test/js/switch_lights.js
+++ test/js/switch_lights.js
@@ -1,5 +1,17 @@
-describe('On the lights grid', function() {
-    it('I can switch one light', function() {
-        expect(true).toEqual(true);
-    });
+describe('On a 1x1 grid', function() {
+    it('I can turn on one light');
+
+    it('I can turn off one light');
+
+    it('I can toggle one light');
+
+    it('I can send a sequence of commands');
+});
+
+describe('On a 1000x1000 grid', function() {
+    it('I can turn on every light');
+
+    it('I can turn off a bunch of lights wherever I want');
+
+    it('I can send the Santa\'s sequence');
 });
